74951b4447dc9393eafe3f84be319c55dbf73542
struartapp <info@struartapp.com>
2024-06-25 12:45:02 +0100

    21st commit 25.06.24

--- js/app.js
+++ js/app.js
@@ -1,9 +1,20 @@
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker.register('sw.js', { scope: '/sip/' })
+//   .then(function(registration) {
+//     console.log('Service Worker registered with scope:', registration.scope);
+//   })
+//   .catch(function(error) {
+//     console.log('Service Worker registration failed:', error);
+//   });
+// }
+
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js', { scope: '/sip/' })
-  .then(function(registration) {
-    console.log('Service Worker registered with scope:', registration.scope);
-  })
-  .catch(function(error) {
-    console.log('Service Worker registration failed:', error);
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('sw.js')
+      .then(registration => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      }).catch(error => {
+        console.log('Service Worker registration failed:', error);
+      });
   });
 }
--- sw.js
+++ sw.js
@@ -1,19 +1,16 @@
-const cacheName = 'cache-v1';
-const request = new Request('https://third-party-no-cors.com/', {
-  mode: 'no-cors',
-});
-
-const cacheAssets = [
+const staticCacheName = 'site-static-v2';
+const dynamicCacheName = 'site-dynamic-v1';
+const assets = [
   '/sip',
   'index.html',
- 'homepage.html',
+  'homepage.html',
   'floordeflection.html',
   'deflection.html',
   'js/app.js',
   'js/ui.js',
   'css/output.css',
   'css/input.css',
-  'tailwind.config.js', 
+  'tailwind.config.js',
   'img/230113-SIPS-CHP2-images-Fig-2-3-2-Roof-Panel-Example-Bending-App-Elevation_Fig 2.3.1 Slope Length of Panel copy.jpg',
   'img/230113-SIPS-CHP2-images-Fig-2-3-2-Roof-Panel-Example-Bending-APP-Section.jpg',
   'img/240228-floor-panel-32-32-32.jpg',
@@ -21,61 +18,133 @@ const cacheAssets = [
   'img/240301_Panel_Sections_Standard_Reinforced.jpg',
   'img/240301_Two_or_more_span_panel_elevation-33.jpg',
   'img/app-background.jpg',
-  'fallback.html'
-];
+  'fallback.html',
+  'img/512x512app-icon.png'
 
-// Call install event
-self.addEventListener('install', e => {
-  console.log('Service Worker: Installed');
+  ];
 
-  e.waitUntil(
-    caches  
-      .open(cacheName)
-      .then(cache => {
-        console.log('Service Worker: Caching Files');
-        cache.addAll(cacheAssets);
-      })
-      .then(() => self.skipWaiting())
+// cache size limit function
+const limitCacheSize = (name, size) => {
+  caches.open(name).then(cache => {
+    cache.keys().then(keys => {
+      if(keys.length > size){
+        cache.delete(keys[0]).then(limitCacheSize(name, size));
+      }
+    });
+  });
+};
 
+// install event
+self.addEventListener('install', evt => {
+  //console.log('service worker installed');
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
   );
-
 });
 
-// Call Active Event
+// activate event
+self.addEventListener('activate', evt => {
+  //console.log('service worker activated');
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      //console.log(keys);
+      return Promise.all(keys
+        .filter(key => key !== staticCacheName && key !== dynamicCacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
 
-// self.addEventListener('activate', e => {
-//   console.log('Service Worker: Activated');
-//   //remove unwanted caches
+// fetch event
+self.addEventListener('fetch', evt => {
+  //console.log('fetch event', evt);
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request).then(fetchRes => {
+        return caches.open(dynamicCacheName).then(cache => {
+          cache.put(evt.request.url, fetchRes.clone());
+          // check cached items size
+          limitCacheSize(dynamicCacheName, 15);
+          return fetchRes;
+        })
+      });
+    }).catch(() => {
+      if(evt.request.url.indexOf('.html') > -1){
+        return caches.match('fallback.html');
+      } 
+    })
+  );
+});
+
+
+
+//24.06.24 VERSION ONLINE
+
+// const cacheName = 'cache-v1';
+// const cacheAssets = [
+//   '/sip',
+//   'index.html',
+//   'homepage.html',
+//   'floordeflection.html',
+//   'deflection.html',
+//   'js/app.js',
+//   'js/ui.js',
+//   'css/output.css',
+//   'css/input.css',
+//   'tailwind.config.js',
+//   'img/230113-SIPS-CHP2-images-Fig-2-3-2-Roof-Panel-Example-Bending-App-Elevation_Fig 2.3.1 Slope Length of Panel copy.jpg',
+//   'img/230113-SIPS-CHP2-images-Fig-2-3-2-Roof-Panel-Example-Bending-APP-Section.jpg',
+//   'img/240228-floor-panel-32-32-32.jpg',
+//   'img/240301_Colon_Cover-royal-app-front-coverAPP.jpg',
+//   'img/240301_Panel_Sections_Standard_Reinforced.jpg',
+//   'img/240301_Two_or_more_span_panel_elevation-33.jpg',
+//   'img/app-background.jpg',
+//   'fallback.html',
+//   'img/512x512app-icon.png'
+// ];
+
+// // Install event
+// self.addEventListener('install', e => {
+//   console.log('Service Worker: Installed');
 //   e.waitUntil(
-//     caches.keys().then(cacheNames => {
-//       return Promise.all(
-//         cacheNames.map(cache =>{
-//           if(cache !== cacheName) {
-//             console.log('Service worker: Clearing Old Cache');
-//             return caches.delete(cache);
-//           }
-
-//         })
-//       )
-//     })
+//     caches.open(cacheName)
+//       .then(cache => {
+//         console.log('Service Worker: Caching Files');
+//         return cache.addAll(cacheAssets);
+//       })
+//       .then(() => self.skipWaiting())
 //   );
-
 // });
 
-fetch('https://struartapp.com/sip', {
-  mode: 'no-cors'
-})
-  .then(response => console.log(response))
-  .catch(error => console.error(error));
+// // Activate event
+// // self.addEventListener('activate', e => {
+// //   console.log('Service Worker: Activated');
+// //   e.waitUntil(
+// //     caches.keys().then(cacheNames => {
+// //       return Promise.all(
+// //         cacheNames.map(cache => {
+// //           if (cache !== cacheName) {
+// //             console.log('Service Worker: Clearing Old Cache');
+// //             return caches.delete(cache);
+// //           }
+// //         })
+// //       );
+// //     })
+// //   );
+// // });
 
 
-//Call Fetch Event
-self.addEventListener ('fetch', e =>{
-  console.log('Service Worker: Fetching');
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request))
-  )
-});
 
-// Assume `cache` is an open instance of the Cache class.
-// fetch(request).then(response => cache.put(request, response));
+
+// // Fetch event
+// self.addEventListener('fetch', e => {
+//   console.log('Service Worker: Fetching');
+//   e.respondWith(
+//     fetch(e.request).catch(() => caches.match(e.request))
+//   );
+// });
 
